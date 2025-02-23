263fa1c935d1a8efa50a7e52e8e502e1e4ff2d25
MAO <leejy19@163.com>
2025-02-23 14:59:47 +0800

    add CS61A folder

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CS61A
 CS61A course
+## lab0 log
+**2025-02-23**
---
+++ lab/lab0/lab00/lab00.py
@@ -0,0 +1,15 @@
+def twenty_twenty_five():
+    """Come up with the most creative expression that evaluates to 2025
+    using only numbers and the +, *, and - operators (or ** and % if you'd like).
+
+    >>> twenty_twenty_five()
+    2025
+    """
+    
+    num = 0
+    twenty = 20
+    twenty_five = 5 ** 2
+    num = twenty * 100 + twenty_five
+    # num = twenty * 100 - twenty_five
+    return num
+
